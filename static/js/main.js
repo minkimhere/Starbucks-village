4eb3cecf4052bd94a9b409407ee4a2e4825fc1c8
Ricky <ricky0813@naver.com>
2022-01-11 16:24:52 +0900

    Add all

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function sortCoffee(selected) {
-    if (selected == "모두보기") {
-        $(`#review-card`).addClass('review-open')
-    } else {
-        $(`#review-card`).addClass('review-close')
-    }
-}
